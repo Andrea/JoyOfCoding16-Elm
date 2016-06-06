243f526e89ce33cec6839daa13474c2dc50b6c27
Michael Twomey <michael.twomey@udemy.com>
2016-06-06 19:34:24 +0100

    Add a Cat.elm to show different animation states

---
+++ Cat.elm
@@ -0,0 +1,161 @@
+module CollisionExample exposing (..)
+
+{-| Cut down game, with level and basic collision detection
+
+Main character is an exciting box
+-}
+
+import Element
+import Html
+import Html.App
+import Keyboard.Extra
+
+
+type Direction
+    = Right
+    | Left
+
+
+type State
+    = Standing
+    | Running
+    | Jumping
+
+
+type alias Cat =
+    { state : State
+    , direction : Direction
+    }
+
+
+type alias Model =
+    { cat : Cat
+    , keyboardModel : Keyboard.Extra.Model
+    }
+
+
+type Msg
+    = KeyboardExtraMsg Keyboard.Extra.Msg
+
+
+init : ( Model, Cmd Msg )
+init =
+    let
+        ( keyboardModel, keyboardCmd ) =
+            Keyboard.Extra.init
+
+        model =
+            { cat = Cat Standing Right
+            , keyboardModel = keyboardModel
+            }
+    in
+        model
+            ! [ Cmd.map KeyboardExtraMsg keyboardCmd
+              ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        KeyboardExtraMsg keyboardMsg ->
+            let
+                ( keyboardModel, keyboardCmd ) =
+                    Keyboard.Extra.update keyboardMsg model.keyboardModel
+
+                arrows =
+                    Keyboard.Extra.arrows keyboardModel
+                        |> Debug.log "arrows"
+
+                cat =
+                    model.cat
+
+                -- Note: Inferring the animation state directly from keyboard presses is kinda clunky.
+                -- Better to update a model (e.g. direction and velocity) and render based on that.
+                ( runningState, direction ) =
+                    case Keyboard.Extra.arrowsDirection keyboardModel of
+                        Keyboard.Extra.East ->
+                            ( Running, Right )
+
+                        Keyboard.Extra.West ->
+                            ( Running, Left )
+
+                        _ ->
+                            ( Standing, cat.direction )
+
+                -- In particular here, if we're mid air and let go of space the cat returns to a
+                -- running or standing state.
+                jumpingState =
+                    case Keyboard.Extra.isPressed Keyboard.Extra.Space keyboardModel of
+                        True ->
+                            Jumping
+
+                        False ->
+                            Standing
+
+                state =
+                    case ( runningState, jumpingState ) of
+                        ( Standing, Jumping ) ->
+                            Jumping
+
+                        ( Running, Standing ) ->
+                            Running
+
+                        ( Running, Jumping ) ->
+                            Jumping
+
+                        ( _, _ ) ->
+                            Standing
+
+                newCat =
+                    { cat | direction = direction, state = state }
+            in
+                { model | cat = newCat, keyboardModel = keyboardModel } ! [ Cmd.map KeyboardExtraMsg keyboardCmd ]
+
+
+renderCat : Model -> Element.Element
+renderCat model =
+    let
+        ( state, extension ) =
+            case model.cat.state of
+                Running ->
+                    ( "running", ".gif" )
+
+                Standing ->
+                    ( "standing", ".png" )
+
+                Jumping ->
+                    ( "jumping", ".png" )
+
+        direction =
+            case model.cat.direction of
+                Left ->
+                    "left"
+
+                Right ->
+                    "right"
+    in
+        Element.image 100 100 ("images/cat-" ++ state ++ "-" ++ direction ++ extension)
+
+
+view : Model -> Html.Html Msg
+view model =
+    Html.div []
+        [ renderCat model |> Element.toHtml
+        ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ Sub.map KeyboardExtraMsg Keyboard.Extra.subscriptions
+        ]
+
+
+main : Program Never
+main =
+    Html.App.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
