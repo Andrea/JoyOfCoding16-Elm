b5db69344f30f56173b1d82c529d3ae3a17f57aa
Roundcrisis <roundcrisis@gmail.com>
2016-06-13 22:06:27 +0100

    hello cat world

---
+++ 01-HelloCatWorld.elm
@@ -0,0 +1,48 @@
+module HelloCatWorld exposing (..)
+
+{-| Gluing all the bits together :)
+-}
+
+import Element
+import Html
+import Html.App
+
+type alias Model = { cat : String}
+
+type Msg = Nothing -- TODO:  get rid of this?
+
+view : Model -> Html.Html Msg
+view model =
+
+        Html.div []
+            [ Element.layers
+                [ Element.tiledImage 150 160 model.cat
+
+                ]
+                |> Element.toHtml
+            ]
+
+init :  Model
+init =
+    let
+        model =
+            { cat = "images/cat-standing-left.gif"
+            }
+    in
+        model
+
+update : Msg -> Model -> Model
+update msg model =
+    let
+      _ = Debug.log "msg " msg
+      _ = Debug.log "msg " model.cat
+    in
+      model
+
+main : Program Never
+main =
+    Html.App.beginnerProgram
+        { model = init
+        , update = update
+        , view = view
+        }
